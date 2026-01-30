1b171158c9fe0715a0d015f29db65b92c03cce79
Amit Rajput <amitrajput98267313@gmail.com>
2026-01-30 20:13:08 +0530

    feat(auth): add auth routes and wire into app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 
 const app = express();
 app.use(cors());
